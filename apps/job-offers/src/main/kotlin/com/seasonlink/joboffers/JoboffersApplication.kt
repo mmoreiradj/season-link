acd42b3916461e80a9e94a84fdb5062d3c59f168
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2023-11-27 17:36:29 +0100

    feat(joboffers): bootstrap and implement joboffers service

---
+++ apps/job-offers/src/main/kotlin/com/seasonlink/joboffers/JoboffersApplication.kt
@@ -0,0 +1,11 @@
+package com.seasonlink.joboffers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobOffersApplication
+
+fun main(args: Array<String>) {
+    runApplication<JobOffersApplication>(*args)
+}
